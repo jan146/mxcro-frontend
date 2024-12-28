d41b4b2e17d3ccb68fc2354069702c19e916b0b7
jan146 <anurjan@gmail.com>
2024-12-28 14:07:50 +0100

    Improve text field

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,7 @@
     <div class="input">
         <div class="font-bold text-lg text-white">Enter user_id:</div>
         <div class="flex gap-2">
-            <input type="text" class="text-lg w-24" placeholder="123"/>
+            <input type="text" class="text-lg w-24 text-black" placeholder="123" maxlength="6"/>
             <button type="button" class="bg-gray-500 text-base border-4 border-white rounded-md text-white p-1 font-semibold">Submit</button>
         </div>
     </div>

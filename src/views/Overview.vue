98ef3cafba6740f57d96fb93aee3792140ef515e
jan146 <anurjan@gmail.com>
2024-12-28 23:07:16 +0100

    Added table template

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,20 +1,36 @@
 <template>
-    <div class="w-full">
-        <h1>This is the overview page for user with user_id: {{$route.params.user_id}}</h1>
-        <div class="flex flex-col w-full" v-if="username">
-            <div>Username: {{username}}</div>
-            <div>Age: {{age}}</div>
-            <div>Height: {{height}} cm</div>
-            <div>Weight: {{weight}} kg</div>
+    <div class="w-full flex gap-4">
+        <div>
+            <h1>This is the overview page for user with user_id: {{$route.params.user_id}}</h1>
+            <div class="flex flex-col w-full" v-if="username">
+                <div>Username: {{username}}</div>
+                <div>Age: {{age}}</div>
+                <div>Height: {{height}} cm</div>
+                <div>Weight: {{weight}} kg</div>
+            </div>
+            <div v-else-if="online">
+                <div>Failed to get information about specified user</div> 
+            </div>
+            <div v-else-if="offline">
+                <div>Failed to connect to backend. Is it up?</div> 
+            </div>
+            <div v-else>
+                <div>Connecting to server ...</div> 
+            </div>
         </div>
-        <div v-else-if="online">
-            <div>Failed to get information about specified user</div> 
-        </div>
-        <div v-else-if="offline">
-            <div>Failed to connect to backend. Is it up?</div> 
-        </div>
-        <div v-else>
-            <div>Connecting to server ...</div> 
+        <div class="grid grid-cols-3 gap-0 custom-border">
+            <div class="col-span-1 custom-border">Arrow left</div>
+            <div class="col-span-1 custom-border">{{date}}</div>
+            <div class="col-span-1 custom-border">Arrow right</div>
+            <div class="col-span-2 row-span-3">
+                <div class="col-span-2 custom-border">Entry</div>
+                <div class="col-span-2 custom-border">Entry</div>
+                <div class="col-span-2 custom-border">Entry</div>
+            </div>
+            <div class="col-span-1 row-span-3 bg-green-400 custom-border">Add entry</div>
+            <!-- <div class="col-span-1 row-span-3 bg-blue-200 custom-border">Add entry</div> -->
+            <!-- <div class="col-span-1 bg-green-200 custom-border">Main Content</div> -->
+            <!-- <div class="col-span-1 bg-red-200 custom-border">Footer</div> -->
         </div>
     </div>
 </template>
@@ -32,6 +48,7 @@
     const weight: Ref<string> = ref("");
     const online: Ref<boolean> = ref(false);
     const offline: Ref<boolean> = ref(false);
+    const date: string = new Date().toLocaleString("en-GB", { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' });
 
     onMounted(() => {
         fetch(`${BACKEND_URL}/user_info/${route.params.user_id}`)
@@ -53,3 +70,8 @@
 
 </script>
 
+<style scoped>
+.custom-border {
+  @apply border border-4 border-gray-100;
+}
+</style>

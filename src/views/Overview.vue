38d0de7e1f66cb34703e3b99b0789287bacace21
jan146 <anurjan@gmail.com>
2024-12-28 12:21:50 +0100

    Added boilerplate for overview page

---
+++ src/views/Overview.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+      <h1>This is the overview page for user with user_id: {{$route.params.user_id}}</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>

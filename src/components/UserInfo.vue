3c7dd793870d0c525f0d0e20846684dc35867197
jan146 <anurjan@gmail.com>
2024-12-29 19:14:03 +0100

    Moved user info into separate component

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <h1>This is the overview page for user with user_id: {{$route.params.user_id}}</h1>
+        <div class="flex flex-col w-full" v-if="username">
+            <div>Username: {{username}}</div>
+            <div>Age: {{age}}</div>
+            <div>Height: {{height}} cm</div>
+            <div>Weight: {{weight}} kg</div>
+        </div>
+        <div v-else-if="online">
+            <div>Failed to get information about specified user</div> 
+        </div>
+        <div v-else-if="offline">
+            <div>Failed to connect to backend. Is it up?</div> 
+        </div>
+        <div v-else>
+            <div>Connecting to server ...</div> 
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+    import { useRoute, type RouteLocationNormalizedLoaded as Route } from "vue-router";
+    import { ref, type Ref, onMounted } from 'vue';
+    import { BACKEND_URL } from '@/utils/constants';
+
+    const route: Route = useRoute();
+    const username: Ref<string> = ref("");
+    const age: Ref<string> = ref("");
+    const height: Ref<string> = ref("");
+    const weight: Ref<string> = ref("");
+    const online: Ref<boolean> = ref(false);
+    const offline: Ref<boolean> = ref(false);
+
+    onMounted(() => {
+        fetch(`${BACKEND_URL}/user_info/${route.params.user_id}`)
+            .then(response => response.json())
+            .then(data => {
+                online.value = true;
+                if (!data.error) {
+                    username.value = data["username"];
+                    age.value = data["age"];
+                    height.value = data["height"];
+                    weight.value = data["weight"];
+                }
+            })
+            .catch(err => {
+                offline.value = true;
+                console.error(err);
+            })
+    })
+
+</script>
+
--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,22 +1,7 @@
 <template>
     <div class="w-full flex gap-4">
         <div>
-            <h1>This is the overview page for user with user_id: {{$route.params.user_id}}</h1>
-            <div class="flex flex-col w-full" v-if="username">
-                <div>Username: {{username}}</div>
-                <div>Age: {{age}}</div>
-                <div>Height: {{height}} cm</div>
-                <div>Weight: {{weight}} kg</div>
-            </div>
-            <div v-else-if="online">
-                <div>Failed to get information about specified user</div> 
-            </div>
-            <div v-else-if="offline">
-                <div>Failed to connect to backend. Is it up?</div> 
-            </div>
-            <div v-else>
-                <div>Connecting to server ...</div> 
-            </div>
+            <UserInfo />
         </div>
         <div>
             <DailyView />
@@ -25,37 +10,7 @@
 </template>
 
 <script setup lang="ts">
-
-    import { useRoute, type RouteLocationNormalizedLoaded as Route } from "vue-router";
-    import { ref, type Ref, onMounted } from 'vue';
-    import { BACKEND_URL } from '@/utils/constants';
     import DailyView from "@/components/DailyView.vue";
-
-    const route: Route = useRoute();
-    const username: Ref<string> = ref("");
-    const age: Ref<string> = ref("");
-    const height: Ref<string> = ref("");
-    const weight: Ref<string> = ref("");
-    const online: Ref<boolean> = ref(false);
-    const offline: Ref<boolean> = ref(false);
-
-    onMounted(() => {
-        fetch(`${BACKEND_URL}/user_info/${route.params.user_id}`)
-            .then(response => response.json())
-            .then(data => {
-                online.value = true;
-                if (!data.error) {
-                    username.value = data["username"];
-                    age.value = data["age"];
-                    height.value = data["height"];
-                    weight.value = data["weight"];
-                }
-            })
-            .catch(err => {
-                offline.value = true;
-                console.error(err);
-            })
-    })
-
+    import UserInfo from "@/components/UserInfo.vue";
 </script>
 

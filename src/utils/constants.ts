37e83ee5e84879426de93cf6671a36d37c50dc73
jan146 <anurjan@gmail.com>
2024-12-31 20:26:17 +0100

    RDAs and order of nutrients

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,33 @@
 
+interface StringToNumber {
+    [key: string]: number;
+}
+
 export const BACKEND_URL: string = "http://localhost:5000/";
+export const NUTRIENTS_ORDERED: Array<string> = [
+    "calories",
+    "fat_total",
+    "fat_saturated",
+    "carbohydrates",
+    "fiber",
+    "sugar",
+    "protein",
+    "sodium",
+    "potassium",
+    "cholesterol",
+];
+// source: https://www.fda.gov/media/99059/download
+// source: https://www.heart.org/en/healthy-living/healthy-eating/eat-smart/sodium/potassium
+export const NUTRIENTS_RDA: StringToNumber = {
+    "calories": 2000,
+    "fat_total": 78,
+    "fat_saturated": 20,
+    "carbohydrates": 275,
+    "fiber": 28,
+    "sugar": 50,
+    "protein": 50,
+    "sodium": 2300,
+    "potassium": 3000,
+    "cholesterol": 300,
+};
 
